6d86e95a1a84d78324fa0257fe3573a59c657b45
RickFleischmann <rickf@careercruising.com>
2016-12-30 10:49:13 -0500

    pre entity framework changes

--- CodeProjectAngular.WebAPI/Controllers/RestaurantsController.cs
+++ CodeProjectAngular.WebAPI/Controllers/RestaurantsController.cs
@@ -11,30 +11,36 @@ namespace CodeProjectAngular.WebAPI.Controllers
 {
     public class RestaurantsController : ApiController
     {
-        //// GET api/values
-        //public IEnumerable<Restaurant> Get()
-        //{
-    
-        //    OdeToFoodDb _db = new OdeToFoodDb();
-            
-        //    var model = _db.Restaurants.ToList();
-
-        //    return model;
-
-        // }
+        OdeToFoodDb _db = new OdeToFoodDb();
 
         // GET api/values
-        public IEnumerable<MoviesDTO> Get()
+        public IEnumerable<Restaurant> Get()
         {
-            List<MoviesDTO> movies = new List<MoviesDTO>
-                {
-                    new MoviesDTO { Id = 1, Name = "Pacifist Club", Director = "David Fincher" },
-                    new MoviesDTO { Id = 2, Name = "Into The Wild", Director = "Sean Penn" },
-                    new MoviesDTO { Id = 3, Name = "Dancer in the Dark", Director = "Lars von Trier" },
-                };
-
-            return movies;
+            var model = _db.Restaurants.ToList();
+            return model;
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null){
+                _db.Dispose();
+            }
+            
+            base.Dispose(disposing);
+        }
+
+        //// GET api/values
+        //public IEnumerable<MoviesDTO> Get()
+        //{
+        //    List<MoviesDTO> movies = new List<MoviesDTO>
+        //        {
+        //            new MoviesDTO { Id = 1, Name = "Pacifist Club", Director = "David Fincher" },
+        //            new MoviesDTO { Id = 2, Name = "Into The Wild", Director = "Sean Penn" },
+        //            new MoviesDTO { Id = 3, Name = "Dancer in the Dark", Director = "Lars von Trier" },
+        //        };
+
+        //    return movies;
+        //}
 
     }
 }
